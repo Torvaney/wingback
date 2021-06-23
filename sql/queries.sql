520e136e72ff1c2a687875860f2bb49ab65ba4d0
Ben Torvaney <torvaney@protonmail.com>
2021-06-23 16:48:36 +0100

    Add ns-xG team strengths

--- sql/queries.sql
+++ sql/queries.sql
@@ -30,6 +30,8 @@ select
   match.*,
   match_xg.home_xg as naive_home_xg,
   match_xg.away_xg as naive_away_xg,
+  fte_match.home_nsxg,
+  fte_match.away_nsxg,
   home.title as home_team,
   away.title as away_team,
   (:end)::date - kickoff::date as days_ago
@@ -40,15 +42,19 @@ join team as away
   on away.id = match.away_team_id
 left join match_xg
   on match_xg.match_id = match.id
+left join fte_match
+  on fte_match.home_team_id = match.home_team_id
+ and fte_match.away_team_id = match.away_team_id
+ and fte_match.date = match.kickoff::date
   -- Supplying None (i.e. NULL) to any of the arguments
   -- will result in the corresponding where clause being
   -- ignored (i.e. it will coalesce to TRUE)
-where coalesce(kickoff::date >= :start, true)
-  and coalesce(kickoff::date < :end, true)
-  and coalesce(league_id in :league_ids, true)
-  and coalesce(season_id in :season_ids, true)
+where coalesce(match.kickoff::date >= :start, true)
+  and coalesce(match.kickoff::date < :end, true)
+  and coalesce(match.league_id in :league_ids, true)
+  and coalesce(match.season_id in :season_ids, true)
   -- Constant clauses
-  and is_result = true
+  and match.is_result = true
 order by kickoff
 
 
@@ -72,3 +78,19 @@ where model = :model
   and league_id in :league_ids
   and date = (:date)::date
 limit 1
+
+
+
+-- :name fetch_understat_teams :many
+select * from team
+
+
+
+-- :name fetch_fivethirtyeight_teams :many
+with team as (
+  select distinct team1 as team from fte_match where league = 'Barclays Premier League'
+  union all
+  select distinct team2 as team from fte_match where league = 'Barclays Premier League'
+)
+
+select distinct team from team
